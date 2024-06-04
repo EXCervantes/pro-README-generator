9d85a6b2bbf5e645174c0e74d2dc432843bbfadd
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-06-03 23:24:47 -0500

    Reformatted JS files and removed test badge

--- index.js
+++ index.js
@@ -4,104 +4,113 @@ const colors = require("colors");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// Inquire module to take in user data and then generate the README markdown
+// Questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+        validate: function (answer) {
+            if (answer.length > 1) return true;
+            return console.log("Please enter a valid title for your project.");
+        }
+    },
+    {
+        type: "input",
+        message: "Write a description of your project",
+        name: "description",
+        validate: function (answer) {
+            if (answer.length > 1) return true;
+            return console.log("Please enter a description for your project.");
+        }
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project for the Installation section?",
+        name: "install",
+        default: "N/A",
+    },
+    {
+        type: "input",
+        message: "What are the steps required to use your project for the Usage section?",
+        name: "usage",
+        validate: function (answer) {
+            if (answer.length > 1) return true;
+            return console.log("Please enter a how to use your project.");
+        }
+    },
+    {
+        type: "input",
+        message: "How would you like to contribute to this project?",
+        name: "contribute",
+        default: "N/A",
+    },
+    {
+        type: "input",
+        message: "What are some of your tests for your project to be listed under Tests?",
+        name: "tests",
+        default: "N/A",
+    },
+    {
+        type: "input",
+        message: "What is you github username? (Please omit the @)",
+        name: "github",
+        validate: function (answer) {
+            if (answer.length > 1) return true;
+            return console.log("A valid GitHub username is required.");
+        }
+    },
+    {
+        type: "input",
+        message: "What is you email address?",
+        name: "email",
+        validate: function (answer) {
+            if (answer.length > 1) return true;
+            return console.log("A valid email is required.");
+        }
+    },
+    {
+        type: "list",
+        message: "Choose a license for your project.",
+        name: "license",
+        choices: [
+            "None",
+            "Apache License 2.0",
+            "GNU GPLv3",
+            "MIT License",
+            "BSD 2-Clause 'Simplified' License",
+            "BSD 3-Clause 'New' or 'Revised' License",
+            "Boost Software License 1.0",
+            "Creative Commons Zero v1.0 Universal",
+            "Eclipse Public License 2.0",
+            "GNU AGPLv3",
+            "GNU GPLv2",
+            "GNU LGPLv2.1",
+            "Mozilla Public License 2.0",
+            "The Unlicense",
+        ],
+    },
+];
+
+// Write Markdown file to system
+function writeToFile(fileName, data) {
+    const readMe = generateMarkdown(data);
+
+    fs.writeFile(fileName, readMe, (err) =>
+        err
+            ? console.log(colors.bgBrightRed(err))
+            : console.log(colors.bgBrightGreen("Success! Enjoy your shiny new README."))
+    );
+}
+
+// Initialize app and generating README.md
 function init() {
     inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is the title of your project?",
-                name: "title",
-                validate: function (answer) {
-                    if (answer.length > 1) return true;
-                    return console.log("Please enter a valid title for your project.");
-                }
-            },
-            {
-                type: "input",
-                message: "Write a description of your project",
-                name: "description",
-                validate: function (answer) {
-                    if (answer.length > 1) return true;
-                    return console.log("Please enter a description for your project.");
-                }
-            },
-            {
-                type: "input",
-                message: "What are the steps required to install your project for the Installation section?",
-                name: "install",
-                default: "N/A",
-            },
-            {
-                type: "input",
-                message: "What are the steps required to use your project for the Usage section?",
-                name: "usage",
-                validate: function (answer) {
-                    if (answer.length > 1) return true;
-                    return console.log("Please enter a how to use your project.");
-                }
-            },
-            {
-                type: "input",
-                message: "How would you like to contribute to this project?",
-                name: "contribute",
-                default: "N/A",
-            },
-            {
-                type: "input",
-                message: "What are some of your tests for your project to be listed under Tests?",
-                name: "tests",
-                default: "N/A",
-            },
-            {
-                type: "input",
-                message: "What is you github username? (Please omit the @)",
-                name: "github",
-                validate: function (answer) {
-                    if (answer.length > 1) return true;
-                    return console.log("A valid GitHub username is required.");
-                }
-            },
-            {
-                type: "input",
-                message: "What is you email address?",
-                name: "email",
-                validate: function (answer) {
-                    if (answer.length > 1) return true;
-                    return console.log("A valid email is required.");
-                }
-            },
-            {
-                type: "list",
-                message: "Choose a license for your project.",
-                name: "license",
-                choices: [
-                    "None",
-                    "Apache License 2.0",
-                    "GNU GPLv3",
-                    "MIT License",
-                    "BSD 2-Clause 'Simplified' License",
-                    "BSD 3-Clause 'New' or 'Revised' License",
-                    "Boost Software License 1.0",
-                    "Creative Commons Zero v1.0 Universal",
-                    "Eclipse Public License 2.0",
-                    "GNU AGPLv3",
-                    "GNU GPLv2",
-                    "GNU LGPLv2.1",
-                    "Mozilla Public License 2.0",
-                    "The Unlicense",
-                ],
-            },
-        ])
+        .prompt(questions)
         .then((data) => {
-            const readMe = generateMarkdown(data);
-
-            fs.writeFile("DemoREADME.md", readMe, (err) =>
-                err
-                    ? console.log(colors.bgBrightRed(err))
-                    : console.log(colors.bgBrightGreen("Success! Enjoy your shiny new README."))
-            );
+            writeToFile("DemoREADME.md", data)
         });
 }
 
+// Call the initialization function
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,72 +1,105 @@
-// Generate License badge and text for License section
-function licenseBadge(data) {
-    const licenseType = data.license;
+// Generate License badge and URL
+function licenseBadge(license) {
     let licenseUrlString = ""
+    if (license === "None") {
+        licenseUrlString = ""
+    }
+    if (license === "Apache License 2.0") {
+        licenseUrlString = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    };
+    if (license === "GNU GPLv3") {
+        licenseUrlString = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    };
+    if (license === "MIT License") {
+        licenseUrlString = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    };
+    if (license === "BSD 2-Clause 'Simplified' License") {
+        licenseUrlString = `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`
+    }
+    if (license === "BSD 3-Clause 'New' or 'Revised' License") {
+        licenseUrlString = `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+    }
+    if (license === "Boost Software License 1.0") {
+        licenseUrlString = `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+    }
+    if (license === "Creative Commons Zero v1.0 Universal") {
+        licenseUrlString = `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)`
+    }
+    if (license === "Eclipse Public License 2.0") {
+        licenseUrlString = `[![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://opensource.org/license/EPL-2.0)`
+    }
+    if (license === "GNU AGPLv3") {
+        licenseUrlString = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`
+    }
+    if (license === "GNU General Public License 2.0") {
+        licenseUrlString = `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+    };
+    if (license === "GNU LGPLv2.1") {
+        licenseUrlString = `[![License: LGPL v2.1](https://img.shields.io/badge/License-LGPL_v2.1-blue.svg)](https://www.gnu.org/licenses/old-licenses/lgpl-2.1)`
+    }
+    if (license === "Mozilla Public License 2.0") {
+        licenseUrlString = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+    }
+    if (license === "The Unlicense") {
+        licenseUrlString = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+    }
+    return licenseUrlString;
+};
+
+// Generate License section
+function renderLicenseSection(license) {
     let licenseSection = ""
-    if (licenseType === "None") {
-        licenseUrlString = `![License: None](assets/images/License-None.jpg)`
+    if (license === "None") {
         licenseSection = "This project is not covered by any license."
     }
-    if (licenseType === "Apache License 2.0") {
-        licenseUrlString = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](https://opensource.org/licenses/Apache-2.0) page for more info.`
+    if (license === "Apache License 2.0") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](https://opensource.org/licenses/Apache-2.0) page for more info.`
     };
-    if (licenseType === "GNU GPLv3") {
-        licenseUrlString = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](https://www.gnu.org/licenses/gpl-3.0) page for more info.`
+    if (license === "GNU GPLv3") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](https://www.gnu.org/licenses/gpl-3.0) page for more info.`
     };
-    if (licenseType === "MIT License") {
-        licenseUrlString = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](https://opensource.org/licenses/MIT) page for more info.`
+    if (license === "MIT License") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](https://opensource.org/licenses/MIT) page for more info.`
     };
-    if (licenseType === "BSD 2-Clause 'Simplified' License") {
-        licenseUrlString = `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](https://opensource.org/licenses/BSD-2-Clause) page for more info.`
+    if (license === "BSD 2-Clause 'Simplified' License") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](https://opensource.org/licenses/BSD-2-Clause) page for more info.`
     }
-    if (licenseType === "BSD 3-Clause 'New' or 'Revised' License") {
-        licenseUrlString = `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](https://opensource.org/licenses/BSD-3-Clause) page for more info.`
+    if (license === "BSD 3-Clause 'New' or 'Revised' License") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](https://opensource.org/licenses/BSD-3-Clause) page for more info.`
     }
-    if (licenseType === "Boost Software License 1.0") {
-        licenseUrlString = `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](https://www.boost.org/LICENSE_1_0.txt) page for more info.`
+    if (license === "Boost Software License 1.0") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](https://www.boost.org/LICENSE_1_0.txt) page for more info.`
     }
-    if (licenseType === "Creative Commons Zero v1.0 Universal") {
-        licenseUrlString = `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](http://creativecommons.org/publicdomain/zero/1.0/) page for more info.`
+    if (license === "Creative Commons Zero v1.0 Universal") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](http://creativecommons.org/publicdomain/zero/1.0/) page for more info.`
     }
-    if (licenseType === "Eclipse Public License 2.0") {
-        licenseUrlString = `[![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://opensource.org/license/EPL-2.0)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](https://opensource.org/license/EPL-2.0) page for more info.`
+    if (license === "Eclipse Public License 2.0") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](https://opensource.org/license/EPL-2.0) page for more info.`
     }
-    if (licenseType === "GNU AGPLv3") {
-        licenseUrlString = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](https://www.gnu.org/licenses/agpl-3.0) page for more info.`
+    if (license === "GNU AGPLv3") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](https://www.gnu.org/licenses/agpl-3.0) page for more info.`
     }
-    if (licenseType === "GNU General Public License 2.0") {
-        licenseUrlString = `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html) page for more info.`
+    if (license === "GNU General Public License 2.0") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html) page for more info.`
     };
-    if (licenseType === "GNU LGPLv2.1") {
-        licenseUrlString = `[![License: LGPL v2.1](https://img.shields.io/badge/License-LGPL_v2.1-blue.svg)](https://www.gnu.org/licenses/old-licenses/lgpl-2.1)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](https://www.gnu.org/licenses/old-licenses/lgpl-2.1) page for more info.`
+    if (license === "GNU LGPLv2.1") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](https://www.gnu.org/licenses/old-licenses/lgpl-2.1) page for more info.`
     }
-    if (licenseType === "Mozilla Public License 2.0") {
-        licenseUrlString = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](https://opensource.org/licenses/MPL-2.0) page for more info.`
+    if (license === "Mozilla Public License 2.0") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](https://opensource.org/licenses/MPL-2.0) page for more info.`
     }
-    if (licenseType === "The Unlicense") {
-        licenseUrlString = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
-        licenseSection = `This project is licensed under the ${data.license}. Please see the [License](http://unlicense.org/) page for more info.`
+    if (license === "The Unlicense") {
+        licenseSection = `This project is licensed under the ${license}. Please see the [License](http://unlicense.org/) page for more info.`
     }
-    return { licenseUrlString, licenseSection };
-};
+    return licenseSection;
+}
+
 
 // Generate the Markdown structure
 function generateMarkdown(data) {
     return `# ${data.title}
     
-${licenseBadge(data).licenseUrlString}
+${licenseBadge(data.license)}
     
 ## Description
   
@@ -92,7 +125,7 @@ ${data.usage}
   
 ## License
   
-${licenseBadge(data).licenseSection}
+${renderLicenseSection(data.license)}
 
 ## Contributing
   
